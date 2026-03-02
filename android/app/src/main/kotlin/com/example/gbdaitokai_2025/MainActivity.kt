f5f057f196b83caa37bfac2ae03a767f3ef4dbe7
くらもとやすひろ <kuramoto@pscsrv.co.jp>
2026-03-02 13:54:10 +0900

    🎉 Flutterプロジェクトを合同勉強会資料をベースに作成

---
+++ android/app/src/main/kotlin/com/example/gbdaitokai_2025/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.okayama_dot_flutter_1st
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:okayama_dot_flutter_1st/app.dart';
+
+void main() {
+  // Flutter Initialize
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const App());
+}
